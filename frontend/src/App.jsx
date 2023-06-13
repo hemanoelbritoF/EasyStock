a89aaa15177ff010de9265811f7e335f32938c6d
hemanoelbritoF <hemanoel.britoF@protonmail.com>
2023-06-12 22:43:14 -0300

    react atualizar bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { GlobalStyled } from "./GlobalStyled.js";
 //App,jsx vai praticamente virar as rotas entre os componentes
 import { AuthProvider } from "./contexts/AuthContext.jsx";
-import { Outlet } from "react-router-dom";
+import { Outlet, Router } from "react-router-dom";
 
 function App() {
     return (
+        <Router basename ="/EasyStock">
         <AuthProvider>
             <GlobalStyled />
             <Outlet />
         </AuthProvider>
+        </Router>
     );
 }
 
